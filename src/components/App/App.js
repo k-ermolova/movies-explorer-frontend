df7f24d61273a7574173e040fa617fe68a6e5422
k-ermolova <ermolowaksenia@yandex.ru>
2021-08-31 22:29:20 +0300

    refactor: add promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import Header from '../Header/Header';
 import './App.css';
+import Main from "../Main/Main";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import Promo from "../Promo/Promo";
+import './Main.css';
+
+function Main() {
+  return(
+  <Promo />
+  );
+}
+
+export default Main;
