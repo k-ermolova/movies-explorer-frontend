dd5aed600b76f7efdc0b473650dfd3ecd61bff0e
k-ermolova <ermolowaksenia@yandex.ru>
2021-09-02 19:37:49 +0300

    feat: add techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import './Main.css';
+import Techs from "../Techs/Techs";
 
 function Main() {
   return (
     <main className='content'>
       <Promo/>
       <AboutProject/>
+      <Techs/>
     </main>
   );
 }
