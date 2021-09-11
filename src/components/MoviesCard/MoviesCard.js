0a55788ace4057889ea42e9600d7accdbc5fcb2b
k-ermolova <ermolowaksenia@yandex.ru>
2021-09-11 23:35:05 +0300

    feat: add movies

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,9 +1,14 @@
 import './MoviesCard.css';
 
-function MoviesCard() {
+function MoviesCard(props) {
   return(
     <li className='card'>
-
+      <div className='card__heading'>
+        <h3 className='card__title'>В погоне за Бенкси</h3>
+        <p className='card__duration'>27 минут</p>
+      </div>
+      <img className='card__image' src={props.image} alt='Постер к фильму'/>
+      {props.saved ? <button className='card__button card__button_saved'/> : <button className='card__button'>Сохранить</button>}
     </li>
   )
 }
