31b6edac9fc9f05b5a38c1ef7b29c54cd30c65db
k-ermolova <ermolowaksenia@yandex.ru>
2021-09-24 23:52:59 +0300

    fix: saved movies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,9 +1,11 @@
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 
+import './SavedMovies.css';
+
 function SavedMovies() {
   return(
-    <main className='saved-movies'>
+    <main className='saved-movies page__saved-movies'>
       <SearchForm/>
       <MoviesCardList type='saved'/>
     </main>
