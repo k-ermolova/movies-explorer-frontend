025fecb5ef20b3aed0821b9213627c984644376d
k-ermolova <ermolowaksenia@yandex.ru>
2021-10-14 23:46:38 +0300

    refactor: form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,9 @@
-import Form from '../Form/Form';
-import {useState} from "react";
+import {Link} from 'react-router-dom';
+import {useState} from 'react';
+
+import Logo from '../Logo/Logo';
+
+import './Login.css';
 
 function Login(props) {
   const [userData, setUserData] = useState({
@@ -12,7 +16,24 @@ function Login(props) {
     evt.preventDefault();
     props.onLogin({email, password})
   }
-  return (<Form component='login'/>);
+  return (
+    <div className='auth page__auth'>
+      <Logo/>
+      <h3 className='auth__title'>Рады видеть!</h3>
+      <form className='form'>
+        <fieldset className='form__field'>
+          <label className='form__label'>E-mail</label>
+          <input className='input-text input-text_type_login' type='email' required/>
+          <label className='form__label'>Пароль</label>
+          <input className='input-text input-text_type_login' type='password' required/>
+        </fieldset>
+        <button className='form__button' type='submit'>Войти</button>
+        <p className='form__question'>Ещё не зарегистрированы?&nbsp;
+          <Link to='/signup' className='form__link'>Регистрация</Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
+import {Link} from 'react-router-dom';
 
-import Form from '../Form/Form';
+import Logo from '../Logo/Logo';
 
 function Register(props) {
   const [userData, setUserData] = useState({
@@ -15,7 +16,26 @@ function Register(props) {
     props.onRegister({name, email, password});
   }
 
-  return (<Form component='register'/>);
+  return (
+    <div className='auth page__auth'>
+      <Logo/>
+      <h3 className='auth__title'>Добро пожаловать!</h3>
+      <form className='form'>
+        <fieldset className='form__field'>
+          <label className='form__label'>Имя</label>
+          <input className='input-text' type='text' required/>
+          <label className='form__label'>E-mail</label>
+          <input className='input-text' type='email' required/>
+          <label className='form__label'>Пароль</label>
+          <input className='input-text' type='password' required/>
+        </fieldset>
+        <button className='form__button' type='submit'>Зарегистрироваться</button>
+        <p className='form__question'>Уже зарегистрированы?&nbsp;
+          <Link to='/signin' className='form__link'>Войти</Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
