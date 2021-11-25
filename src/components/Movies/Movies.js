a66cbc3638f5881c1504c3af018955d697bb08d3
k-ermolova <ermolowaksenia@yandex.ru>
2021-11-25 17:35:06 +0300

    feat: add functional to show-more button

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,13 +1,39 @@
+import {useMemo, useState} from 'react';
+
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 import './Movies.css';
 
+let initialNum;
+let showNum;
+
+const setVisibleCards = () => {
+  if (window.innerWidth >= 1280) {
+    initialNum = 12;
+    showNum = 3;
+  } else if (window.innerWidth >= 768) {
+    initialNum = 8;
+    showNum = 2;
+  } else {
+    initialNum = 5;
+    showNum = 2;
+  }
+}
+
 function Movies(props) {
+  const [visible, setVisible] = useState(initialNum);
+
+  const showMoreCards = () => {
+    setVisible(previous => previous + showNum);
+  }
+
+  useMemo(() => setVisibleCards(), []);
+
   return(
   <main className='movies'>
-    <SearchForm onChange={props.onChange} onSubmit={props.onSubmit}/>
-    <MoviesCardList cards={props.cards}/>
+    <SearchForm onChange={props.onChange} onSubmit={props.onSubmit} validation={props.validation}/>
+    <MoviesCardList cards={props.cards} onShowMore={showMoreCards} visible={visible}/>
   </main>
   );
 }
