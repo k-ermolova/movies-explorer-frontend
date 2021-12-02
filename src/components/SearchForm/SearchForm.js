6e12e9c2473de6e47fb479e944c30c5499c8b3c8
k-ermolova <ermolowaksenia@yandex.ru>
2021-12-02 18:53:40 +0300

    refactor: search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,81 @@
+import {useState} from 'react';
+
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
 import './SearchForm.css';
 
 function SearchForm(props) {
-  return(
+  const [value, setValue] = useState({
+    saved: localStorage.valueForSaved ? JSON.parse(localStorage.valueForSaved) : '',
+    all: localStorage.value ? JSON.parse(localStorage.value) : '',
+  });
+  const [error, setError] = useState('');
+
+  const handleChange = (evt) => {
+    if (props.saved) {
+      setValue({
+        ...value,
+        saved: evt.target.value,
+      });
+      if (value.saved === '') {
+        setError('Нужно ввести ключевое слово.');
+      }
+    } else {
+      setValue({
+        ...value,
+        all: evt.target.value,
+      });
+      if (value.all === '') {
+        setError('Нужно ввести ключевое слово.');
+      }
+    }
+  }
+
+  const checkContent = (title, input) => {
+    return title.toLowerCase().includes(input.toLowerCase());
+  }
+
+  const filteredCards = (cards, searchValue) => {
+    return cards.filter((value) => {
+      if (checkContent(value.nameRU, searchValue) || (value.nameEN !== null && checkContent(value.nameEN, searchValue))) {
+        return value;
+      }
+      return '';
+    });
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (props.saved) {
+      const savedMovies = filteredCards(props.savedCards, value.saved);
+      props.setSavedCards(savedMovies);
+      if (!savedMovies.length) {
+        props.setSavedCards(() => props.savedCards);
+      }
+      localStorage.setItem('savedMovies', JSON.stringify(savedMovies));
+      localStorage.setItem('valueForSaved', JSON.stringify(value.saved));
+    } else {
+      const allMovies = filteredCards(props.cards, value.all);
+      props.setMovies(allMovies);
+      localStorage.setItem('allMovies', JSON.stringify(allMovies));
+      localStorage.setItem('value', JSON.stringify(value.all));
+    }
+  }
+
+  const checkError = () => {
+    if (props.saved) {
+      return !value.saved
+    } else return !value.all;
+  }
+
+  return (
     <section className='search movies__search'>
       <div className='search__container'>
-        <form className='search__form' onSubmit={props.onSubmit}>
-          <input className='search__input' placeholder='Фильм' required onChange={props.onChange}/>
-          <button className='search__button' type='submit'>Найти</button>
+        <form className='search__form' onSubmit={handleSubmit}>
+          <input className='search__input' placeholder='Фильм' required onChange={handleChange} type='search'
+                 name='search' value={props.saved ? value.saved : value.all}/>
+          <p className={`search__error ${checkError() ? 'search__error_visible' : ''}`}>{error}</p>
+          <button className='search__button' type='submit' disabled={checkError()}>Найти</button>
         </form>
         <FilterCheckbox onChange={props.onCheckbox} checked={props.checked} value=''/>
       </div>
